4a3e9ddd3477e6b49f164ffbabf10cdbb62767b9
nobuko.m <animopara100pre@gmail.com>
2020-08-25 02:49:53 +0000

    [finish]dotenvrails_railsc_railsdbseed

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -9,36 +9,22 @@ class TodolistsController < ApplicationController
     # ２. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # ３. トップ画面へリダイレクト
-    redirect_to todolist_path(list.id)
+    redirect_to '/top'
   end
-  
+    
   def index
-    @lists = List.all
-  end 
-
-def show
-  @list = List.find(params[:id])
-end
-
-def edit 
-  @list = List.find(params[:id])
-end
-
-def update
-  list = List.find(params[:id])
-    list.update(list_params)
-    redirect_to todolist_path(list.id)
-end
-
-  def destroy
-    list = List.find(params[:id])  
-    list.destroy 
-    redirect_to todolists_path
+    puts "作成したキー #{ENV['SECRET_KEY']}"
+    @lists = List.all 
   end
+    
+   def show
+    @list = List.find(params[:id])  
+   end
+  
   
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body, :image)
+    params.require(:list).permit(:title, :body,)
   end
 end
